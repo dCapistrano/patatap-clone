87c5cbd698ddde232efe3a6dd4718f576296a5fc
Daryll Capistrano <daryllcapistrano@gmail.com>
2019-03-08 18:46:28 -0800

    Add files via upload

---
+++ myScripts.js
@@ -0,0 +1,17 @@
+var circles = [];
+
+tool.onKeyDown = function (event) {
+  var maxPoint = new Point(view.size.width, view.size.height);
+  var randomPoint = Point.random();
+  var point = maxPoint * randomPoint;
+  var newCircle = new Path.Circle(point, 500);
+  newCircle.fillColor = "orange"
+  circles.push(newCircle);
+};
+
+function onFrame(event) {
+  for (var i = 0; i < circles.length; i++) {
+    circles[i].fillColor.hue += 1;
+    circles[i].scale(.9);
+  }
+} 
